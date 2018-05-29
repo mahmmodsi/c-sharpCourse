4fa228331b961c399bea9550df76e210c9540d7c
apple <apple@APPLE-PC>
2018-05-29 21:38:17 +0300

    Add a comment

---
+++ My New App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_New_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hollo wworld");
+            Console.ReadKey();
+        }
+    }
+}
